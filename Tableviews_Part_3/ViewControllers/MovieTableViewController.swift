0e73b89f35769924613675a84fd6535382f6d4ff
Sabrina Ip <kkip90@gmail.com>
2016-09-28 16:28:59 -0400

    My stuff

--- Tableviews_Part_3/ViewControllers/MovieTableViewController.swift
+++ Tableviews_Part_3/ViewControllers/MovieTableViewController.swift
@@ -14,7 +14,7 @@ class MovieTableViewController: UITableViewController {
         case action
         case drama
     }
-  
+    
     internal var movieData: [Movie]?
 
     internal let rawMovieData: [[String : Any]] = movies
@@ -145,5 +145,57 @@ class MovieTableViewController: UITableViewController {
         let filtered = movieData?.filter(filter).sorted { $0.year < $1.year }
         
         return filtered
+    }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        // 1. check sender for the cell that was tapped
+        if let tappedMovieCell: MovieTableViewCell = sender as? MovieTableViewCell {
+            
+            // 2. check for the right storyboard segue
+
+            if segue.identifier == "MovieDetailSegue" {
+                
+                // 3. get reference to the destination view controller
+                let movieDetailViewController: MovieDetailViewController = segue.destination as! MovieDetailViewController
+                
+                // 4. get our cell's indexPath
+                let cellIndexPath: IndexPath = self.tableView.indexPath(for: tappedMovieCell)!
+                
+                // 5. get our cell's Movie
+                guard let genre = Genre.init(rawValue: cellIndexPath.section),
+                    let data = byGenre(genre) else {
+                        return
+                }
+                
+                // 6. set the destionation's selectedMovie property
+                let selectedMovie: Movie = data[cellIndexPath.row]
+                movieDetailViewController.selectedMovie = selectedMovie
+            }
+            
+            if segue.identifier == "MovieCastDetailSegue" {
+                
+                // 3. get reference to the destination view controller
+                let movieCastDetailViewController: MovieCastDetailViewController = segue.destination as! MovieCastDetailViewController
+                
+                // 4. get our cell's indexPath
+                let cellIndexPath: IndexPath = self.tableView.indexPath(for: tappedMovieCell)!
+                
+                // 5. get our cell's Movie
+                guard let genre = Genre.init(rawValue: cellIndexPath.section),
+                    let data = byGenre(genre) else {
+                        return
+                }
+                
+                // 6. set the destionation's selectedMovie property
+                let selectedMovie: Movie = data[cellIndexPath.row]
+                movieCastDetailViewController.selectedMovie = selectedMovie
+            }
+        }
     }
 }
